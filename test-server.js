8d0247e56146afc6c14670b235976748cd8a77f4
TheatreKnitter <ecolemansound@gmail.com>
2017-01-29 18:47:38 +0000

    testing travis

---
+++ test-server.js
@@ -0,0 +1,15 @@
+// JavaScript File
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(chaiHttp);
+
+describe('hello world', function() {
+    it('print "Hello World!"'); 
+});
