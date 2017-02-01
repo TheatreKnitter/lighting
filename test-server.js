2a0c75f827843e5110c41e04c2359c624907ed6a
TheatreKnitter <ecolemansound@gmail.com>
2017-02-01 00:24:27 +0000

    test fix

--- test-server.js
+++ test-server.js
@@ -10,6 +10,14 @@ var storage = server.storage;
 
 chai.use(chaiHttp);
 
-describe('hello world', function() {
-    it('print "Hello World!"'); 
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
 });
