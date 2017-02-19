a7003dd3dcd0bb5e7587ed088c4fbc3ec2f6c334
TheatreKnitter <ecolemansound@gmail.com>
2017-02-19 13:40:43 +0000

    attempt 1 at app.js, before Lee

--- public/app.js
+++ public/app.js
@@ -1,1 +1,37 @@
-// JavaScript File
+$(document).ready(function(){
+    
+});
+
+var getInstruments = function(instrument) {
+
+	
+
+	$.ajax({
+		dataType: 'json',
+		type: "Get",
+		url:"",
+	})
+
+	.done(function(result){ 
+		$.each(result.Similar.Results, function(instrument) {
+			var user = showInstruments(instrument);
+			$('table').append(user);
+		});
+	});
+};
+
+var showInstruments = function(instrument){
+	var template = $('.templates').clone();
+	template.find('.results').append('<td class="Id"></td>');
+	template.find('.Id').text(instrument.id);
+	template.find('.results').append('<td class="Model"></td>');
+	template.find('.Model').text(instrument.model);
+	template.find('.results').append('<td class="Company"></td>');
+	template.find('.Company').text(instrument.company);
+	template.find('.results').append('<td class="location"></td>');
+	template.find('.Location').text(instrument.loc);
+
+	$(".search").append(template);
+	template.removeClass("templates");
+	template.removeClass("hitden");
+};
