444476d9479ad335ab82acb8f1179c0cb09b2748
TheatreKnitter <ecolemansound@gmail.com>
2017-03-30 13:50:12 +0000

    fixing

--- public/app.js
+++ public/app.js
@@ -51,8 +51,9 @@ var showInstruments = function(state){
 function createNewInstrument(state) {
     var item = {itemNum: $('#new-id').val(), model: $('#new-model').val(), company: $('#new-company').val(), loc: $('#new-location').val()};
     var ajax = $.ajax('/instruments', {
-        type: 'POST',
+        method: 'POST',
         data: JSON.stringify(item),
+        contentType: 'application/json',
         dataType: 'json',
     });
     ajax.done(state.currentInstruments.push(item));
